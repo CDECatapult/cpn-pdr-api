07901c0a2d1c62d311f0a9cf35c91f0a431e816b
Anthony Garcia <anthony.garcia@digicatapult.org.uk>
2019-02-22 14:44:47 +0000

    Add MAILGUN_DOMAIN

--- src/env.js
+++ src/env.js
@@ -1,4 +1,4 @@
-const { cleanEnv, str, url } = require('envalid')
+const { cleanEnv, host, str, url } = require('envalid')
 
 module.exports = cleanEnv(process.env, {
   LOG_LEVEL: str({ default: 'info', devDefault: 'debug' }),
@@ -6,7 +6,7 @@ module.exports = cleanEnv(process.env, {
   MAILGUN_API_KEY: str({
     example: '94b00922dfa91f2fc1573896c71e373d-47217ca8-837a15ba',
   }),
-  // MAILGUN_DOMAIN: host({ example: 'projectcpn.eu' }),
+  MAILGUN_DOMAIN: host({ example: 'projectcpn.eu' }),
   MAIL_FROM: str({
     example: 'CPN <postmaster@projectcpn.eu>',
   }),
